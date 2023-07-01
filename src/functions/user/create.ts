f8b045a221dde3798f519eda543fe8ab5de2b2c3
DanielWTE <daniel.iuser@icloud.com>
2023-07-01 12:55:17 +0200

    Added recovery code safe to database

--- src/functions/user/create.ts
+++ src/functions/user/create.ts
@@ -20,6 +20,7 @@ export async function handleCreateUserRequest(req: Request, res: Response) {
     let password = req.body.password;
     let confirmPassword = req.body.confirmPassword;
     let solution = req.body.solution;
+    let recovery_key = req.body.recoveryCode;
 
     if (!username || !domain || !password || !confirmPassword) {
         return res.status(400).json({ message: "Please fill in all fields" });
@@ -115,6 +116,7 @@ export async function handleCreateUserRequest(req: Request, res: Response) {
       password: passwordHashed,
       private_key: encryptedPrivateKey,
       public_key: publicKey,
+      recovery_key: recovery_key,
     });
 
     await newUser.save();
