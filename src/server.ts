c5b56fb19eb4f6f4781eb2c6919a9a52763676af
DanielWTE <daniel.iuser@icloud.com>
2023-07-02 12:23:46 +0200

    added recovery

--- src/server.ts
+++ src/server.ts
@@ -56,6 +56,7 @@ import { handleJWTDetailsUserRequest } from './functions/user/jwt_details';
 import { handleLoginUserRequest } from './functions/user/login';
 import { handleUserDetailsUserRequest } from './functions/user/user_details';
 import { handleValidatePWDUserRequest } from './functions/user/validate_pwd';
+import { handleRecoveryUserRequest } from './functions/user/recovery';
 
 import { handleCheckRecoveryCodeRequest } from './functions/user/forgot/check_recovery_code';
 import { handleResetPasswordRequest } from './functions/user/forgot/reset_password';
@@ -182,6 +183,7 @@ app.post('/user/jwt_details', limiter, auth, jsonParser, handleJWTDetailsUserReq
 app.post('/user/login', limiter, auth, jsonParser, handleLoginUserRequest);
 app.post('/user/user_details', limiter, auth, jsonParser, handleUserDetailsUserRequest);
 app.post('/user/validate_pwd', limiter, auth, jsonParser, handleValidatePWDUserRequest);
+app.post('/user/recovery', limiter, auth, jsonParser, handleRecoveryUserRequest);
 
 app.post('/user/check_recovery_code', limiter, auth, jsonParser, handleCheckRecoveryCodeRequest);
 app.post('/user/reset_password', limiter, auth, jsonParser, handleResetPasswordRequest);
