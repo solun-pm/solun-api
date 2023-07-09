5a4807953749a02eff251fbdd50383d43c6a84c3
DanielWTE <daniel.iuser@icloud.com>
2023-07-09 11:31:09 +0200

    Change smth in get user alias domains

--- src/functions/user/alias/get_domains.ts
+++ src/functions/user/alias/get_domains.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { dbConnect, findOneDocument, User, User_Domains } from 'solun-database-package';
+import { dbConnect, findDocuments, findOneDocument, User, User_Domains } from 'solun-database-package';
 
 export async function handleGetDomainsAliasRequest(req: Request, res: Response) {
   try {
@@ -8,17 +8,21 @@ export async function handleGetDomainsAliasRequest(req: Request, res: Response) 
     let user_id = req.body.user_id;
 
     const user = await findOneDocument(User, { user_id: user_id });
-    const user_domains = await findOneDocument(User_Domains, { user_id: user_id });
+    const user_domains = await findDocuments(User_Domains, { user_id: user_id });
 
-    if (user == null) {
+    if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
 
     const domains = ['@solun.pm', '@6crypt.com', '@seal.pm', '@xolus.de', '@cipher.pm'];
     
-    if (user_domains && user_domains.domains) {
-      user_domains.domains.forEach((domain: any) => {
-        domains.push(`@${domain}`);
+    if (user_domains && user_domains.length) {
+      user_domains.forEach((user_domain: any) => {
+        if (user_domain.domains && user_domain.domains.length) {
+          user_domain.domains.forEach((domain: any) => {
+            domains.push(`@${domain}`);
+          });
+        }
       });
     }
 
