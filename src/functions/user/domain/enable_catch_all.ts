ec3154458656491e835d1f8646e6d4a928fee7f9
DanielWTE <daniel.iuser@icloud.com>
2023-07-16 19:05:56 +0200

    removed for loop

--- src/functions/user/domain/enable_catch_all.ts
+++ src/functions/user/domain/enable_catch_all.ts
@@ -65,17 +65,6 @@ export async function handleEnableCatchAllRequest(req: Request, res: Response) {
     if (!addAlias) {
         return res.status(500).json({ message: "Something went wrong" });
     }
-    /*for (let i = 0; i < forwardingAddresses.length; i++) {
-        const addAlias = await mcc.addAlias({
-            active: 1,
-            address: '@'+domainName,
-            goto: [forwardingAddresses[i]],
-        });
-    
-        if (!addAlias) {
-            return res.status(500).json({ message: "Something went wrong" });
-        }
-    }*/
 
     await User_Domains.updateOne(
         { user_id: user_id, _id: domain_id },
