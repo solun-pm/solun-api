d5baf8f9b6e7f138bcdd3282b023fcadb039b75b
DanielWTE <daniel.iuser@icloud.com>
2023-09-07 12:39:20 +0200

    logging

--- src/functions/user/get_api_details.ts
+++ src/functions/user/get_api_details.ts
@@ -5,6 +5,7 @@ import { findOneDocument, api_keys } from "solun-database-package";
 export async function handleApiDetailsUserRequest(req: Request, res: Response) {
   try {
     const token = req.body.token;
+    console.log(token);
 
     const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY as string;
 
@@ -13,9 +14,13 @@ export async function handleApiDetailsUserRequest(req: Request, res: Response) {
     }
 
     const token_content = jwt.verify(token, JWT_SECRET_KEY);
+    console.log(token_content);
     const user_id = (<any>token_content).user_id;
+    console.log(user_id);
 
     const data = await findOneDocument(api_keys, {user_id: user_id});
+
+    console.log(data);
 
     return res.status(200).json({message: "Api access details retrieved successfully", data: data});
   } catch (error) {
