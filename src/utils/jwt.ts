6914a2b80ef7e614755ababfa171c5451e3b5bbc
DanielWTE <daniel.iuser@icloud.com>
2024-04-08 17:25:32 +0200

    Missing '_KEY' at the end of an env variable

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 export function getJWTData(token: string) {
-    const jwt_secret = process.env.JWT_SECRET as string;
+    const jwt_secret = process.env.JWT_SECRET_KEY as string;
     try {
         const decoded = jwt.verify(token, jwt_secret);
         return decoded;
